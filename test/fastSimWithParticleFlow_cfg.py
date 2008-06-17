06262380bd283a3415c06002eebbc2ffc511c681
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2008-06-17 17:06:47 +0000

    cleaning in the configuration files. cff moved to ../data/

---
+++ test/fastSimWithParticleFlow_cfg.py
@@ -0,0 +1,55 @@
+import FWCore.ParameterSet.Config as cms
+
+
+process = cms.Process("PROD")
+
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+#generation
+process.load("RecoParticleFlow.Configuration.source_singleTau_cfi")
+
+#fastsim
+process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
+process.load("FastSimulation.Configuration.CommonInputsFake_cff")
+process.load("FastSimulation.Configuration.FamosSequences_cff")
+
+process.famosSimHits.SimulateCalorimetry = True
+process.famosSimHits.SimulateTracking = True
+process.famosPileUp.PileUpSimulator.averageNumber = 0.0
+
+process.famosSimHits.VertexGenerator.BetaStar = 0.00001
+process.famosSimHits.VertexGenerator.SigmaZ = 0.00001
+
+# process.famosSimHits.MaterialEffects.PairProduction = false
+# process.famosSimHits.MaterialEffects.Bremsstrahlung = false
+# process.famosSimHits.MaterialEffects.EnergyLoss = false
+# process.famosSimHits.MaterialEffects.MultipleScattering = false
+# process.famosSimHits.MaterialEffects.NuclearInteraction = false
+
+process.load("RecoParticleFlow.PFBlockProducer.particleFlowSimParticle_cff")
+
+
+process.p1 = cms.Path(
+    process.famosWithEverything +
+    process.particleFlowSimParticle
+    )
+
+
+process.load("FastSimulation.Configuration.EventContent_cff")
+process.aod = cms.OutputModule("PoolOutputModule",
+    process.AODEventContent,
+    fileName = cms.untracked.string('aod.root')
+)
+
+process.load("RecoParticleFlow.Configuration.Display_EventContent_cff")
+process.display = cms.OutputModule("PoolOutputModule",
+    process.DisplayEventContent,
+    fileName = cms.untracked.string('display.root')
+)
+
+process.outpath = cms.EndPath(process.aod + process.display)
+
+#
