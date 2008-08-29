e40ec09dde28e44c0dbdbed8e7167016998414b8
Michele Pioppi <sha1-c466acc115a6fdbe5e72763944d7ffc8f1851f4d@cern.ch>
2008-08-29 14:53:43 +0000

    new cfg for nuclear interaction

--- test/reco2block_full_nuclear_cfg.py
+++ test/reco2block_full_nuclear_cfg.py
@@ -5,25 +5,29 @@ process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FakeConditions_cff")
-
+process.load("RecoParticleFlow.Configuration.RecoParticleFlow_nuclear_cff")
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(10)
 )
 process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring('file:reco.root')
+                            fileNames = cms.untracked.vstring('/store/relval/CMSSW_2_1_4/RelValSinglePiPt10/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V6_v1/0004/625F8CDD-1A6C-DD11-8826-001617DBD316.root')
 )
 
-process.MessageLogger = cms.Service("MessageLogger",
-    rectoblk = cms.untracked.PSet(
-        threshold = cms.untracked.string('INFO')
-    ),
-    destinations = cms.untracked.vstring('rectoblk')
-)
+process.MessageLogger = cms.Service(
+    "MessageLogger",
+    nucl_rectoblk = cms.untracked.PSet(
+    threshold = cms.untracked.string('INFO')),
+    destinations = cms.untracked.vstring('nucl_rectoblk')
+    )
+
+process.Timing =cms.Service("Timing")
+
+
 
 process.dump = cms.EDAnalyzer("EventContentAnalyzer")
 process.block = cms.OutputModule("PoolOutputModule",
-    fileName = cms.untracked.string('blocks_full.root'),
+    fileName = cms.untracked.string('nuclear_block.root'),
     outputCommands = cms.untracked.vstring(
             'drop *',
             'keep recoPFRecHits_*_*_*',
@@ -40,14 +44,16 @@ process.block = cms.OutputModule("PoolOutputModule",
 )
 )
 
-
 process.p = cms.Path(process.siPixelRecHits*
                      process.siStripMatchedRecHits*
                      process.ckftracks*
-                     process.caloTowersPFRec*
-                     process.particleFlowCluster*
-                     process.particleFlowTrack*
-                     process.particleFlowBlock)
+                     process.nuclearRemainingHits*
+                     process.particleFlowTrackWithNuclear*
+                     process.particleFlowBlock
+                     )
+
+
+
 process.outpath = cms.EndPath(process.block)
 
 
