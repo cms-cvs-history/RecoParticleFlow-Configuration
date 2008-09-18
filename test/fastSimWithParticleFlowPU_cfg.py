0e0f50848ad333ee90b8a13ffff1c744f0ae1e98
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2008-09-18 16:42:42 +0000

    ttbar events with pile-up

---
+++ test/fastSimWithParticleFlowPU_cfg.py
@@ -0,0 +1,55 @@
+import FWCore.ParameterSet.Config as cms
+
+
+process = cms.Process("PROD")
+
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+#generation
+process.load("FastSimulation/Configuration/ttbar_cfi")
+
+#fastsim
+process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
+process.load("FastSimulation.Configuration.CommonInputsFake_cff")
+process.load("FastSimulation.Configuration.FamosSequences_cff")
+
+process.famosSimHits.SimulateCalorimetry = True
+process.famosSimHits.SimulateTracking = True
+process.famosPileUp.PileUpSimulator.averageNumber = 5.0
+
+# Parametrized magnetic field (new mapping, 4.0 and 3.8T)
+process.load("Configuration.StandardSequences.MagneticField_40T_cff")
+#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.VolumeBasedMagneticFieldESProducer.useParametrizedTrackerField = True
+
+
+process.famosSimHits.MaterialEffects.PairProduction = False
+process.famosSimHits.MaterialEffects.Bremsstrahlung = False
+process.famosSimHits.MaterialEffects.EnergyLoss = False
+process.famosSimHits.MaterialEffects.MultipleScattering = False
+process.famosSimHits.MaterialEffects.NuclearInteraction = False
+
+
+process.p1 = cms.Path(
+    process.famosWithEverything *
+    process.caloJetMetGen 
+    )
+
+process.load("FastSimulation.Configuration.EventContent_cff")
+process.aod = cms.OutputModule("PoolOutputModule",
+    process.AODSIMEventContent,
+    fileName = cms.untracked.string('aod.root')
+)
+
+process.load("RecoParticleFlow.Configuration.Display_EventContent_cff")
+process.display = cms.OutputModule("PoolOutputModule",
+    process.DisplayEventContent,
+    fileName = cms.untracked.string('display.root')
+)
+
+process.outpath = cms.EndPath(process.aod + process.display)
+
+
