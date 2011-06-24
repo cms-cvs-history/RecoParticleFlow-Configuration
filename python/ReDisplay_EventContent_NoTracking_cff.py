cdb9305e542c69adf3c16d2710a3e12dc944dda0
Daniele Benedetti <sha1-b78b7786b135946381de7085dafdd1b4e3149539@cern.ch>
2011-06-24 16:40:37 +0000

    add GsfElectron

--- python/ReDisplay_EventContent_NoTracking_cff.py
+++ python/ReDisplay_EventContent_NoTracking_cff.py
@@ -43,7 +43,9 @@ DisplayEventContent = cms.PSet(
         'keep *_correctedHybridSuperClusters_*_*',
         'keep *_correctedMulti5x5SuperClustersWithPreshower_*_*',
         'keep recoPhotons_photons_*_*',
-        'keep recoPhotonCores_photonCore_*_*'                                   
+        'keep recoPhotonCores_photonCore_*_*',
+        'keep recoGsfElectrons_*_*_*',
+        'keep recoGsfElectronCores_*_*_*'                
    )
 )
 
